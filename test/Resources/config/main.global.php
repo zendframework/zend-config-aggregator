9e2723f03fe00a17bc0884eb08ec59c374565548
Matthew Weier O'Phinney <matthew@zend.com>
2018-02-26 10:32:39 -0600

    Fixes license docblocks
    - Use https everywhere.
    - .docheader doesn't make sense for new files not forked. We'll need to
      come up with a different future check.

--- test/Resources/config/main.global.php
+++ test/Resources/config/main.global.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-config-aggregator for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-config-aggregator/blob/master/LICENSE.md New BSD License
+ */
 
 return [
     'fruit' => 'banana',
